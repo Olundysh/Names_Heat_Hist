f72235b4587b24842bb0bd7ab20a6beed69ffb68
olundysh <olgavecholga@gmail.com>
2023-02-01 13:57:19 +0300

    Ready answer for Names heatMap and Histogram.

---
+++ HeatmapTask.cs
@@ -0,0 +1,36 @@
+﻿using System;
+ 
+namespace Names
+{
+    internal static class HeatmapTask
+    {
+        public static HeatmapData GetBirthsPerDateHeatmap(NameData[] names)
+        { 
+            var days = new string[30];
+            var months = new string[12];
+            
+            for (var i = 2; i <= 31; i++) 
+                days[i-2] = i.ToString();
+
+            for (var i = 1; i <= 12; i++) 
+                months[i-1] = i.ToString();
+
+            var heat = new double[30,12]; 
+            
+            for (var i = 0; i < 30; i++)
+            {
+                var monthArr = new double[12];
+                foreach (var name in names)
+                {
+                    if (name.BirthDate.Day == i + 2) 
+                        monthArr[name.BirthDate.Month - 1] += 1;
+                }
+
+                for (var j = 0; j < 12; j++) 
+                    heat[i, j] = monthArr[j];
+            }          
+            return new HeatmapData("Пример карты интенсивностей",
+                heat, days, months);
+        }
+    }
+}
---
+++ HistogramTask.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace Names
+{
+    internal static class HistogramTask
+    {
+        public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
+        {
+            var xAxis = new string[31];
+            for (var i = 0; i < xAxis.Length; i++)
+            {
+                xAxis[i] = (i + 1).ToString();
+            }
+
+            var numberBorn = new double[xAxis.Length];
+
+            foreach (var item in names)
+            {
+                if (item.Name == name && item.BirthDate.Day != 1)
+                {
+                    numberBorn[item.BirthDate.Day - 1]++;
+                }
+            }
+            
+            return new HistogramData(
+                string.Format("Рождаемость людей с именем '{0}'", name), 
+                xAxis, numberBorn);
+        }
+    }
+}
